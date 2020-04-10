e63a437ab391ca5a591b8922340ae13cfb8754b2
masterful-array-3850 <crgammage@gmail.com>
2020-04-10 14:46:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value});
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  return object[key] = value
-  return recipes
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
+  return object;
 }
